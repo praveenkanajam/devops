93291a3961adc7d42e075e9d33f0e7114aad9957
praveenkanajam <praveenkumar.kanajam@gmail.com>
2019-12-14 08:41:40 +0530

    d

--- Shell-Scripting/Shell-Scripts/03-Output_redirect.sh
+++ Shell-Scripting/Shell-Scripts/03-Output_redirect.sh
@@ -14,7 +14,7 @@ echo -e "\e[32mcopying Output to out2.txt & error to err.txt\e[0m"
 ls 01* 02* 100* >out2.txt 2>err.txt
 echo "output to out2.txt ::"
 cat out2.txt
-echo "error to error.txt ::""
+echo "error to error.txt ::"
 cat err.txt
 echo "-----------------------------------"
 echo -e " \e[33mcopying Output & error to out.txt \e[0m"
