e5ec583ad2c165ec14d3fc6d74131e4c1887f5f8
praveenkanajam <praveenkumar.kanajam@gmail.com>
2019-12-14 08:36:17 +0530

    d

--- Shell-Scripting/Shell-Scripts/03-Output_redirect.sh
+++ Shell-Scripting/Shell-Scripts/03-Output_redirect.sh
@@ -6,20 +6,20 @@ ls 01* 02* 100*
 echo "----"
 
 ls 01* 02* 100* > out1.txt
-echo " copying output to out1.txt"
+echo " copying output to out1.txt ::"
 cat out1.txt
 echo -e "\e[31m error not copied to out1.txt\e[0m"
 echo " --------------------------------------   "
 echo -e "\e[32mcopying Output to out2.txt & error to err.txt\e[0m"
 ls 01* 02* 100* >out2.txt 2>err.txt
-echo "output to out2.txt"
+echo "output to out2.txt ::"
 cat out2.txt
-echo "error to error.txt"
+echo "error to error.txt ::""
 cat err.txt
 echo "-----------------------------------"
 echo -e " \e[33mcopying Output & error to out.txt \e[0m"
 ls 01* 02* 100* &>out.txt
-echo "output of out.txt file"
+echo "output of out.txt file ::"
 cat out.txt
 echo "________________________________________"
 echo -e "\e[35mNullifiy the output\e[0m"
