b93917e57749e611ddc5fa9030956d5466d5796d
praveenkanajam <praveenkumar.kanajam@gmail.com>
2019-12-14 08:23:55 +0530

    d

---
+++ Shell-Scripting/Shell-Scripts/03-Output_redirect.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+echo "Serching of files 01* 02* 100*" 
+ls 01* 02* 100*
+echo " copying output to out1.txt"
+ls 01* 02* 100* > out1.txt
+cat out1.txt
+echo -e "\e[31m error not copied to out1.txt\e[0m"
+echo " --------------------------------------   "
+echo -e "\e[32mcopying Output to out2.txt & error to err.txt\e[0m"
+ls 01* 02* 100* >out2.txt 2>err.txt
+cat err.txt
+echo "-----------------------------------"
+echo -e " \e[33mcopying Output & error to out.txt \e[0m"
+ls 01* 02* 100* &>out.txt
+cat out.txt
+echo "________________________________________"
+echo -e "\e[35mNullifiy the output\e[0m"
+ls 01* 02* 100* &>/dev/null 
+echo -e "\e[36m Exit status $(echo $?)\e[0m"
